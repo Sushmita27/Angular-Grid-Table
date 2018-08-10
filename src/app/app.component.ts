860d30c829133a05cd3557fc4e1328a5019b30e9
Sushmita Karmakar <sushmitak262@IN.nam.ad.pwcinternal.com>
2018-08-10 12:01:54 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'grid-table';
+  private _opened: boolean = false;
+  private _toggleSidebar() { this._opened = !this._opened; }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { DataTablesModule } from 'angular-datatables';
 import { AppComponent } from './app.component';
+import { GridViewComponent } from './grid-view/grid-view.component';
+import { SidebarModule } from 'ng-sidebar';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    GridViewComponent
+    ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DataTablesModule,
+    SidebarModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
